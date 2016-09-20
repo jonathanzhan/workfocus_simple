52cfa1bf756b7951f944813c41bcff23a8facc20
whatlookingfor <whatlookingfor@gmail.com>
2016-09-20 14:51:05 +0800

    activiti的demo

---
+++ src/main/java/com/mfnets/workfocus/modules/demo/controller/LeaveController.java
@@ -0,0 +1,22 @@
+/**
+ * Copyright &copy; 2012-2016 <a href="https://github.com/whatlookingfor">whatlookingfor</a> All rights reserved.
+ */
+
+package com.mfnets.workfocus.modules.demo.controller;
+
+import com.mfnets.workfocus.common.web.BaseController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 请假工作流的Controller层
+ *
+ * @author Jonathan
+ * @version 2016/9/19 14:34
+ * @since JDK 7.0+
+ */
+@Controller
+@RequestMapping("${adminPath}/demo/leave")
+public class LeaveController extends BaseController {
+
+}
