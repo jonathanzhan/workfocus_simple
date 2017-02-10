287ec941876e5f4eed4c19bb734c4061aa3d233f
Jonathan <whatlookingfor@gmail.com>
2017-02-10 16:04:43 +0800

    restful文件上传

--- src/main/java/com/mfnets/workfocus/modules/demo/controller/RestfulController.java
+++ src/main/java/com/mfnets/workfocus/modules/demo/controller/RestfulController.java
@@ -13,11 +13,16 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
+import java.util.List;
 import java.util.Map;
 
 
@@ -42,6 +47,56 @@ public class RestfulController extends BaseController{
 		result.put("name","Jonathan");
 		return result;
 	}
+
+	//Accept: image/png
+	/**
+	 * 文件上传
+	 * @return
+	 */
+	@RequestMapping(value = "upload",consumes = "multipart/form-data",method = RequestMethod.POST)
+	public Map<String,String> upload(@RequestParam("fileUpload") CommonsMultipartFile file){
+
+		Map<String,String> result = Maps.newHashMap();
+		result.put("id","1");
+		// 判断文件是否存在
+		if (!file.isEmpty()) {
+			result.put("name","上传成功");
+			return result;
+		}
+		result.put("name","上传失败");
+		return result;
+	}
+
+	@RequestMapping(value = "/upload1", consumes = "multipart/form-data", method = RequestMethod.POST)
+	public void picture(HttpServletRequest request) {
+
+		String uploadPicturePath = "/Users/whatlookingfor/Pictures";
+		String pathname = uploadPicturePath + "/";
+		File file = new File(pathname);
+		if (!file.exists()) {
+			file.mkdirs();
+		}
+		MultipartHttpServletRequest muti = (MultipartHttpServletRequest) request;
+		System.out.println(muti.getMultiFileMap().size());
+
+		MultiValueMap<String, MultipartFile> map = muti.getMultiFileMap();
+		for (Map.Entry<String, List<MultipartFile>> entry : map.entrySet()) {
+
+			List<MultipartFile> list = entry.getValue();
+			for (MultipartFile multipartFile : list) {
+
+				try {
+					multipartFile.transferTo(new File(pathname
+							+ multipartFile.getOriginalFilename()));
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+
+			}
+		}
+	}
+
+
 
 
 
